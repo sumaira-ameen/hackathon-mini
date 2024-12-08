934e9218426538aca38aaae85cadefa16862fb86
Sumaira Ameen <sumairaameen0001@gmail.com>
2024-12-08 18:28:03 +0500

    Update firebase.js

--- login-signup/firebase.js
+++ login-signup/firebase.js
@@ -7,13 +7,7 @@ signInWithPopup} from  'https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.
 import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js"
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "hackathon-coranu.firebaseapp.com",
-  projectId: "hackathon-coranu",
-  storageBucket: "hackathon-coranu.firebasestorage.app",
-  messagingSenderId: "694733625117",
-  appId: "1:694733625117:web:50e2d06d928a56bddcf3f4",
-  measurementId: "G-B9DC282E89"
+  //hide
 };
 
 const app = initializeApp(firebaseConfig);
